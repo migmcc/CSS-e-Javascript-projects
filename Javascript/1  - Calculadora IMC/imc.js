13dd13a501abf0ac7eb3c47ba5dea8bd13d45e1c
migmcc <migmcc@gmail.com>
2021-01-08 22:54:17 +0000

    calculadora_IMC

---
+++ Javascript/1  - Calculadora IMC/imc.js
@@ -0,0 +1,11 @@
+
+const nome = document.querySelector('.nome');
+const altura = document.querySelector('.altura');
+const peso = document.querySelector('.peso');
+
+const btn = document.querySelector('.btn');
+let resultado = querySelector('.resultado');
+
+btn.addEventListener('click', () => {
+    
+});
